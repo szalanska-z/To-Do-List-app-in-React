8fafd1b5fd5dfd8152cfb3af5e9efca3b6e30314
Zaneta Szalanska <zaneta.szalanska@gmail.com>
2020-05-25 20:22:08 +0200

    Updates

---
+++ precache-manifest.87ed734fa7ee91e608f3c7f923ebdddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991bd046547a3f0eff1a7a318fc024ea",
+    "url": "/To-Do-List-app-in-React/index.html"
+  },
+  {
+    "revision": "7023c49adaa9f495ffbb",
+    "url": "/To-Do-List-app-in-React/static/css/main.0014f345.chunk.css"
+  },
+  {
+    "revision": "09bc3dac551bc1f8513e",
+    "url": "/To-Do-List-app-in-React/static/js/2.4c54f048.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List-app-in-React/static/js/2.4c54f048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7023c49adaa9f495ffbb",
+    "url": "/To-Do-List-app-in-React/static/js/main.805d6f0f.chunk.js"
+  },
+  {
+    "revision": "5dbff5617119ba505138",
+    "url": "/To-Do-List-app-in-React/static/js/runtime-main.3a8e729e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-List-app-in-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
